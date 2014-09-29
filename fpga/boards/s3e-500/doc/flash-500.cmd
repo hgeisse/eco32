39c4f8e732741b66ecfb909a849a654f1226f353
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2014-09-29 08:32:35 +0000

    flashing the parallel ROM on the s3e starter kit board
    git-svn-id: http://opencores.org/ocsvn/eco32/eco32/trunk@241 133310ea-c0b2-4f2d-ae91-eb8ea07367b3

--- fpga/boards/s3e-500/doc/flash-500.cmd
+++ fpga/boards/s3e-500/doc/flash-500.cmd
@@ -1,7 +1,7 @@
 setMode -bscan
 setCable -p auto
-addDevice -position 1 -part xc3s500e -file ./flash-500.bit
+addDevice -position 1 -file ./flash-500.bit
 addDevice -position 2 -part xcf04s
 addDevice -position 3 -part xc2c64a
-program -e -v -p 1
+program -p 1
 quit
