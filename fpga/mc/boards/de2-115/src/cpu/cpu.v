5a44a22752fd4d9f20512df1e6e4ee0c1135da9e
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2017-02-20 10:21:24 +0100

    add CPU

--- fpga/mc/boards/de2-115/src/cpu/cpu.v
+++ fpga/mc/boards/de2-115/src/cpu/cpu.v
@@ -21,4 +21,46 @@ module cpu(clk, rst,
     input bus_ack;			// bus acknowledge
     input [15:0] bus_irq;		// bus interrupt requests
 
+  wire cpu_stb;
+  wire cpu_we;
+  wire [1:0] cpu_size;
+  wire [31:0] cpu_addr;
+  wire [31:0] cpu_din;
+  wire [31:0] cpu_dout;
+  wire cpu_ack;
+  wire [15:0] cpu_irq;
+
+  cpu_bus cpu_bus_1(
+    .clk(clk),
+    .rst(rst),
+    .bus_stb(bus_stb),
+    .bus_we(bus_we),
+    .bus_addr(bus_addr[31:2]),
+    .bus_din(bus_din[31:0]),
+    .bus_dout(bus_dout[31:0]),
+    .bus_ack(bus_ack),
+    .bus_irq(bus_irq[15:0]),
+    .cpu_stb(cpu_stb),
+    .cpu_we(cpu_we),
+    .cpu_size(cpu_size[1:0]),
+    .cpu_addr(cpu_addr[31:0]),
+    .cpu_din(cpu_din[31:0]),
+    .cpu_dout(cpu_dout[31:0]),
+    .cpu_ack(cpu_ack),
+    .cpu_irq(cpu_irq[15:0])
+  );
+
+  cpu_core cpu_core_1(
+    .clk(clk),
+    .rst(rst),
+    .bus_stb(cpu_stb),
+    .bus_we(cpu_we),
+    .bus_size(cpu_size[1:0]),
+    .bus_addr(cpu_addr[31:0]),
+    .bus_din(cpu_din[31:0]),
+    .bus_dout(cpu_dout[31:0]),
+    .bus_ack(cpu_ack),
+    .bus_irq(cpu_irq[15:0])
+  );
+
 endmodule
