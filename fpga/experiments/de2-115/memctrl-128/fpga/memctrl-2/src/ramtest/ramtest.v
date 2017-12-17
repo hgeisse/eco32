93b3a2655b5ce1082121cba29690f5a5d79557af
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2017-12-17 09:10:15 +0100

    add 128-bit SDRAM controller for FPGA, 50/100 MHz

---
+++ fpga/experiments/de2-115/memctrl-128/fpga/memctrl-2/src/ramtest/ramtest.v
@@ -0,0 +1,266 @@
+//
+// ramtest.v -- RAM test generator
+//
+
+
+`timescale 1ns/10ps
+`default_nettype none
+
+
+//`define SIMULATE
+//`define VERBOSE
+
+`define INST_PERIOD	37
+`define INST_PHASE	19
+`define DATA_PERIOD	17
+`define DATA_PHASE	7
+
+
+//
+// memory test generator
+//
+// Algorithm: Three independent address/data generators
+// produce exactly the same sequence of address/data pairs,
+// although at different times: data write, data read, and
+// instruction read. Three out of four data cycles are writes,
+// one is a read. INST_PERIOD must be set high enough so that
+// instruction reads are also less frequent than data writes,
+// and thus no "don't cares" show up in the instructions read.
+// The exact value depends on the actual number of read/write
+// cycles consumed by the underlying RAM.
+//
+
+module ramtest(clk, rst,
+               inst_stb, inst_addr,
+               inst_din, inst_ack,
+               data_stb, data_we, data_addr,
+               data_dout, data_din, data_ack,
+               test_ended, test_error);
+    input clk;
+    input rst;
+    output reg inst_stb;
+    output [24:0] inst_addr;
+    input [127:0] inst_din;
+    input inst_ack;
+    output reg data_stb;
+    output reg data_we;
+    output [24:0] data_addr;
+    output [127:0] data_dout;
+    input [127:0] data_din;
+    input data_ack;
+    output test_ended;
+    output test_error;
+
+  reg [5:0] inst_timer;
+  reg [4:0] data_timer;
+  reg [9:0] data_counter;
+
+  wire ir_next;
+  wire [22:0] ir_a;
+  wire [127:0] ir_d;
+
+  wire dw_next;
+  wire [22:0] dw_a;
+  wire [127:0] dw_d;
+
+  wire dr_next;
+  wire [22:0] dr_a;
+  wire [127:0] dr_d;
+
+`ifdef SIMULATE
+  reg error_1;
+  reg error_3;
+`endif
+  reg error_2;
+  reg error_4;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      inst_timer <= 0;
+      inst_stb <= 0;
+`ifdef SIMULATE
+      error_1 <= 0;
+`endif
+      error_2 <= 0;
+    end else begin
+      if (~test_ended | inst_stb) begin
+        if (~inst_stb) begin
+          if (inst_timer == `INST_PERIOD - 1) begin
+            inst_timer <= 0;
+          end else begin
+            inst_timer <= inst_timer + 6'd1;
+          end
+          if (inst_timer == `INST_PHASE) begin
+            inst_stb <= 1;
+          end
+        end else begin
+          if (inst_ack) begin
+            inst_stb <= 0;
+`ifdef SIMULATE
+`ifdef VERBOSE
+            $display("%t: inst read  @ 0x%h", $realtime, ir_a);
+            $display("                   value = 0x%h", inst_din);
+`endif
+            if (^inst_din[127:0] === 1'bx) begin
+              $display("Warning: Input data has don't cares at %t",
+                       $realtime);
+              error_1 <= 1;
+            end
+`endif
+            if (inst_din[127:0] != ir_d[127:0]) begin
+              error_2 <= 1;
+            end
+          end
+        end
+      end
+    end
+  end
+
+  adgen adgen_ir(
+    .clk(clk),
+    .rst(rst),
+    .next(ir_next),
+    .addr(ir_a),
+    .data(ir_d)
+  );
+
+  assign ir_next = inst_ack;
+  assign inst_addr[24:0] = { 2'h0, ir_a[22:0] };
+
+  always @(posedge clk) begin
+    if (rst) begin
+      data_timer <= 0;
+      data_stb <= 0;
+      data_we <= 0;
+      data_counter <= 0;
+`ifdef SIMULATE
+      error_3 <= 0;
+`endif
+      error_4 <= 0;
+    end else begin
+      if (~test_ended | data_stb) begin
+        if (~data_stb) begin
+          if (data_timer == `DATA_PERIOD - 1) begin
+            data_timer <= 0;
+          end else begin
+            data_timer <= data_timer + 5'd1;
+          end
+          if (data_timer == `DATA_PHASE) begin
+            data_stb <= 1;
+            data_we <= ~&data_counter[1:0];
+          end
+        end else begin
+          if (data_ack) begin
+            data_stb <= 0;
+            data_we <= 0;
+            data_counter <= data_counter + 10'd1;
+`ifdef SIMULATE
+`ifdef VERBOSE
+            if (data_we == 1) begin
+              $display("%t: data write @ 0x%h", $realtime, dw_a);
+              $display("                   value = 0x%h", dw_d);
+            end else begin
+              $display("%t: data read  @ 0x%h", $realtime, dr_a);
+              $display("                   value = 0x%h", data_din);
+            end
+`endif
+            if (data_we == 0 &&
+                ^data_din[127:0] === 1'bx) begin
+              $display("Warning: Input data has don't cares at %t",
+                       $realtime);
+              error_3 <= 1;
+            end
+`endif
+            if (data_we == 0 &&
+                data_din[127:0] != dr_d[127:0]) begin
+              error_4 <= 1;
+            end
+          end
+        end
+      end
+    end
+  end
+
+  adgen adgen_dw(
+    .clk(clk),
+    .rst(rst),
+    .next(dw_next),
+    .addr(dw_a),
+    .data(dw_d)
+  );
+
+  adgen adgen_dr(
+    .clk(clk),
+    .rst(rst),
+    .next(dr_next),
+    .addr(dr_a),
+    .data(dr_d)
+  );
+
+  assign dw_next = data_ack & data_we;
+  assign dr_next = data_ack & ~data_we;
+  assign data_addr[24:0] = { 2'h0, data_we ? dw_a[22:0] : dr_a[22:0] };
+  assign data_dout[127:0] = dw_d[127:0];
+
+  assign test_ended = &data_counter[9:0];
+
+`ifdef SIMULATE
+  assign test_error = error_1 | error_2 | error_3 | error_4;
+`else
+  assign test_error = error_2 | error_4;
+`endif
+
+endmodule
+
+
+//
+// address & data generator
+//
+// compute pseudo-random 32-bit address
+// and 64-bit data on request
+//
+// addresses are restricted to 23 bits
+// data is non-randomly expanded to 128 bits
+//
+
+module adgen(clk, rst,
+             next, addr, data);
+    input clk;
+    input rst;
+    input next;
+    output [22:0] addr;
+    output [127:0] data;
+
+  reg [31:0] a;
+  reg [63:0] d;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      a[31: 0] <= 32'hC70337DB;
+      d[63:32] <= 32'h7F4D514F;
+      d[31: 0] <= 32'h75377599;
+    end else begin
+      if (next) begin
+        if (a[0] == 0) begin
+          a[31:0] <= a[31:0] >> 1;
+        end else begin
+          a[31:0] <= (a[31:0] >> 1) ^ 32'hD0000001;
+        end
+        if (d[32] == 0) begin
+          d[63:32] <= d[63:32] >> 1;
+        end else begin
+          d[63:32] <= (d[63:32] >> 1) ^ 32'hD0000001;
+        end
+        if (d[0] == 0) begin
+          d[31:0] <= d[31:0] >> 1;
+        end else begin
+          d[31:0] <= (d[31:0] >> 1) ^ 32'hD0000001;
+        end
+      end
+    end
+  end
+
+  assign addr[22:0] = a[22:0];
+  assign data[127:0] = { d[63:0], ~d[63:0] };
+
+endmodule
---
+++ fpga/experiments/de2-115/memctrl-128/fpga/memctrl-2/src/toplevel/memtest.v
@@ -0,0 +1,150 @@
+//
+// memtest.v -- top-level for memory test
+//
+
+
+`timescale 1ns/10ps
+`default_nettype none
+
+
+module memtest(clk_in,
+               rst_in_n,
+               sdram_clk,
+               sdram_cke,
+               sdram_cs_n,
+               sdram_ras_n,
+               sdram_cas_n,
+               sdram_we_n,
+               sdram_ba,
+               sdram_a,
+               sdram_dqm,
+               sdram_dq,
+               led_r,
+               led_g);
+
+    // clock and reset
+    input clk_in;
+    input rst_in_n;
+    // SDRAM
+    output sdram_clk;
+    output sdram_cke;
+    output sdram_cs_n;
+    output sdram_ras_n;
+    output sdram_cas_n;
+    output sdram_we_n;
+    output [1:0] sdram_ba;
+    output [12:0] sdram_a;
+    output [3:0] sdram_dqm;
+    inout [31:0] sdram_dq;
+    // LEDs
+    output [17:0] led_r;
+    output [8:0] led_g;
+
+  // clk_rst
+  wire clk_ok;
+  wire clk2;
+  wire clk;
+  wire rst;
+  // ramtest
+  wire inst_stb;
+  wire [24:0] inst_addr;
+  wire [127:0] inst_to_test;
+  wire inst_ack;
+  wire inst_timeout;
+  wire data_stb;
+  wire data_we;
+  wire [24:0] data_addr;
+  wire [127:0] data_to_ram;
+  wire [127:0] data_to_test;
+  wire data_ack;
+  wire data_timeout;
+  // indicators
+  wire test_ended;
+  wire test_error;
+  reg [25:0] heartbeat2;
+  reg [25:0] heartbeat;
+
+  //
+  // module instances
+  //
+
+  clk_rst clk_rst_1(
+    .clk_in(clk_in),
+    .rst_in_n(rst_in_n),
+    .clk_ok(clk_ok),
+    .clk_100_ps(sdram_clk),
+    .clk_100(clk2),
+    .clk_50(clk),
+    .rst(rst)
+  );
+
+  ramtest ramtest_1(
+    .clk(clk),
+    .rst(rst),
+    .inst_stb(inst_stb),
+    .inst_addr(inst_addr[24:0]),
+    .inst_din(inst_to_test[127:0]),
+    .inst_ack(inst_ack | inst_timeout),
+    .data_stb(data_stb),
+    .data_we(data_we),
+    .data_addr(data_addr[24:0]),
+    .data_dout(data_to_ram[127:0]),
+    .data_din(data_to_test[127:0]),
+    .data_ack(data_ack | data_timeout),
+    .test_ended(test_ended),
+    .test_error(test_error)
+  );
+
+  ramctrl ramctrl_1(
+    .clk_ok(clk_ok),
+    .clk(clk2),
+    .inst_stb(inst_stb),
+    .inst_addr(inst_addr[24:0]),
+    .inst_dout(inst_to_test[127:0]),
+    .inst_ack(inst_ack),
+    .inst_timeout(inst_timeout),
+    .data_stb(data_stb),
+    .data_we(data_we),
+    .data_addr(data_addr[24:0]),
+    .data_din(data_to_ram[127:0]),
+    .data_dout(data_to_test[127:0]),
+    .data_ack(data_ack),
+    .data_timeout(data_timeout),
+    .sdram_cke(sdram_cke),
+    .sdram_cs_n(sdram_cs_n),
+    .sdram_ras_n(sdram_ras_n),
+    .sdram_cas_n(sdram_cas_n),
+    .sdram_we_n(sdram_we_n),
+    .sdram_ba(sdram_ba[1:0]),
+    .sdram_a(sdram_a[12:0]),
+    .sdram_dqm(sdram_dqm[3:0]),
+    .sdram_dq(sdram_dq[31:0])
+  );
+
+  //
+  // indicators
+  //
+
+  always @(posedge clk2) begin
+    heartbeat2[25:0] <= heartbeat2[25:0] + 26'd1;
+  end
+
+  always @(posedge clk) begin
+    heartbeat[25:0] <= heartbeat[25:0] + 26'd1;
+  end
+
+  assign led_r[17:1] = 17'b0;
+  assign led_r[0] = test_error;		// test_error
+
+  assign led_g[8] = 1'b0;
+
+  assign led_g[7] = test_ended;		// test_ended
+  assign led_g[6] = 1'b0;
+  assign led_g[5] = 1'b0;
+  assign led_g[4] = 1'b0;
+  assign led_g[3] = heartbeat2[25];	// heartbeat, 100 MHz
+  assign led_g[2] = heartbeat[25];	// heartbeat, 50 MHz
+  assign led_g[1] = clk_ok;		// clk_ok
+  assign led_g[0] = rst;		// rst
+
+endmodule
