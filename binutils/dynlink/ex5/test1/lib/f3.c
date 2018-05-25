2f478f657c7d7a53136d1888a757bf7611373750
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-05-25 15:39:32 +0200

    add exercise 5 in dynlink

---
+++ binutils/dynlink/ex5/test1/lib/f3.c
@@ -0,0 +1,5 @@
+#include "funcs.h"
+
+int f3(void) {
+  return 3 + f5();
+}
---
+++ binutils/dynlink/ex5/test1/lib/f5.c
@@ -0,0 +1,13 @@
+#include "funcs.h"
+
+char *dummy5_1(void) {
+  return "dummy5_1";
+}
+
+int f5(void) {
+  return 5 + f2();
+}
+
+char *dummy5_2(void) {
+  return "dummy5_2";
+}
