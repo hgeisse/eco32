d34a98ce0e5abc938799ce950759c740e41c7d49
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-06-14 13:03:56 +0200

    example for accessing the GOT

---
+++ binutils/dynlink/pic/example1/m5.s
@@ -0,0 +1,14 @@
+	.export	_start
+
+	.code
+_start:	.gotadr	$1
+	.gotptr	$8,$1,dat3
+	ldw	$8,$8,0
+loop:	j	loop
+
+	.data
+dat1:	.word	0x12345678
+dat2:	.word	0x23456789
+dat3:	.word	0x3456789A
+dat4:	.word	0x456789AB
+dat5:	.word	0x56789ABC
