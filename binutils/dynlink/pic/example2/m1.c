424955c2dc21696d5a05aef0b8eb80068a505326
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-06-25 10:26:09 +0200

    add PIC example in C

--- binutils/dynlink/pic/example2/m1.c
+++ binutils/dynlink/pic/example2/m1.c
@@ -1,14 +1,7 @@
-int a;
-static int b;
-extern int c;
-
-int d = 22;
-static int e = 11;
+static int a;
+extern int b;
 
 void func(void) {
   a = 1;
   b = 2;
-  c = 3;
-  d = 4;
-  e = 5;
 }
---
+++ binutils/dynlink/pic/example2/m2.c
@@ -0,0 +1,14 @@
+int a;
+static int b;
+extern int c;
+
+int d = 22;
+static int e = 11;
+
+void func(void) {
+  a = 1;
+  b = 2;
+  c = 3;
+  d = 4;
+  e = 5;
+}
