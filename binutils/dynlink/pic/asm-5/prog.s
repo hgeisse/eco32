838bc29b30ae9c8169e5bd96ea68ed09d206d615
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-08-07 06:11:16 +0200

    add asm-5: assembler support for accessing global data

---
+++ binutils/dynlink/pic/asm-5/prog.s
@@ -0,0 +1,16 @@
+	.export	_start
+
+	.code
+
+_start:	.gotadr	$3
+	.gotptr	$1,$3,dat3
+	ldw	$8,$1,0
+loop:	j	loop
+
+	.data
+
+dat1:	.word	0x12345678
+dat2:	.word	0x23456789
+dat3:	.word	0x3456789A
+dat4:	.word	0x456789AB
+dat5:	.word	0x56789ABC
