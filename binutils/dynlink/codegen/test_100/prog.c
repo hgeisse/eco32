08474db6a7f8d91ca711abfd2ba8b030ada32229
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-09-04 12:35:30 +0200

    add test_100

---
+++ binutils/dynlink/codegen/test_100/prog.c
@@ -0,0 +1,19 @@
+float f(void) {
+  float f;
+
+  f = 3.14;
+  f = f * f;
+  f = f + f;
+  f = f - f / 2;
+  return f;
+}
+
+double g(void) {
+  double d;
+
+  d = 3.1415;
+  d = d * d;
+  d = d + d;
+  d = d - d / 2;
+  return d;
+}
