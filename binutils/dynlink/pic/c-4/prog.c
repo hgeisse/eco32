35b6dfacf33507f7ebd6580b5e0ee79b41852cbd
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-09-05 11:57:31 +0200

    adapt c-4 to new PIC conventions

--- binutils/dynlink/pic/c-4/prog.c
+++ binutils/dynlink/pic/c-4/prog.c
@@ -1,18 +1,22 @@
 static int a;
 extern int b;
 
+int func1(void) {
+  return 1;
+}
+
 int func2(void) {
-  return 42;
+  return 2;
 }
 
 void func(void) {
-  a = func2();
-  b = func2();
+  a += func1();
+  b += func2();
   a = b;
   b = a;
 }
 
 int main() {
   func();
-  return 0;
+  return a * b;
 }
