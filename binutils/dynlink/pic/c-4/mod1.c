430f32068a6e5381aefad59c4b52e4be89cd06fd
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-09-08 11:42:20 +0200

    enhance test pic/c-4

--- binutils/dynlink/pic/c-4/mod1.c
+++ binutils/dynlink/pic/c-4/mod1.c
@@ -1,1 +1,5 @@
+int g1;
+int g2;
 int b;
+int g3;
+int g4;
--- binutils/dynlink/pic/c-4/prog.c
+++ binutils/dynlink/pic/c-4/prog.c
@@ -1,4 +1,9 @@
+static int s1;
+static int s2;
 static int a;
+static int s3;
+static int s4;
+
 extern int b;
 
 int func1(void) {
@@ -12,7 +17,11 @@ int func2(void) {
 void func(void) {
   a += func1();
   b += func2();
+  /* read access to variable 'b': GOT-indirect */
+  /* write access to varisble 'a': GOT-relative */
   a = b;
+  /* read access to variable 'a': GOT-relative */
+  /* write access to varisble 'b': GOT-indirect */
   b = a;
 }
 
