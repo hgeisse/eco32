f3b84cf5da17224b7f92270f830f08c442194256
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-09-09 10:26:44 +0200

    number all lines which must be checked

--- binutils/dynlink/pic/c-5/prog.c
+++ binutils/dynlink/pic/c-5/prog.c
@@ -6,9 +6,9 @@ static int stat_dummy_1 = 0x11111111;
 static int stat_dummy_3;
 static int stat_dummy_4;
 
-static int m = 0x12345678;
-static int k;
-extern int n;
+static int m = 0x12345678;	/* 1: initialization */
+static int k;			/* 2: initialization */
+extern int n;			/* 3: initialization */
 
 static int stat_dummy_2 = 0x22222222;
 static int stat_dummy_5;
@@ -19,49 +19,49 @@ static void f(void) {
 
 extern void g(void);
 
-/* .data/.code/W32 */
+/* 4: .data/.code/W32 */
 void (*fptr1)(void) = f;
 
-/* .data/.data/W32 */
+/* 5: .data/.data/W32 */
 int *iptr1 = &m;
 
-/* .data/.bss/W32 */
+/* 6: .data/.bss/W32 */
 int *iptr2 = &k;
 
-/* .data/symbol/W32 */
+/* 7: .data/symbol/W32 */
 void (*fptr2)(void) = g;
 
-/* .data/symbol/W32 */
+/* 8: .data/symbol/W32 */
 int *iptr3 = &n;
 
 int main(void) {
   int i;
   void (*fp)(void);
 
-  /* .code/.code/H16, .code/.code/L16 */
+  /* 9: .code/.code/H16, .code/.code/L16 */
   fp = f;
 
-  /* .code/.code/R16 */
+  /* 10: .code/.code/R16 */
   if (m == k) {
     m++;
   }
 
-  /* .code/.code/R26 */
+  /* 11: .code/.code/R26 */
   f();
 
-  /* .code/.data/H16, .code/.data/L16 */
+  /* 12: .code/.data/H16, .code/.data/L16 */
   i = m;
 
-  /* .code/.bss/H16, .code/.bss/L16 */
+  /* 13: .code/.bss/H16, .code/.bss/L16 */
   i = k;
 
-  /* .code/symbol/H16, .code/symbol/L16 */
+  /* 14: .code/symbol/H16, .code/symbol/L16 */
   i = n;
 
-  /* .code/symbol/H16, .code/symbol/L16 */
+  /* 15: .code/symbol/H16, .code/symbol/L16 */
   fp = g;
 
-  /* .code/symbol/R26 */
+  /* 16: .code/symbol/R26 */
   g();
 
   return 0;
