f18741dbed5cdcd6775d2e95a05ae0102eef426f
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-09-09 09:05:17 +0200

    enhance test c-5

--- binutils/dynlink/pic/c-5/prog.c
+++ binutils/dynlink/pic/c-5/prog.c
@@ -3,14 +3,16 @@
  */
 
 static int stat_dummy_1 = 0x1111;
-static int stat_dummy_2 = 0x2222;
 static int stat_dummy_3;
 static int stat_dummy_4;
-static int stat_dummy_5;
 
 static int m = 0x12345678;
 static int k;
 extern int n;
+
+static int stat_dummy_2 = 0x2222;
+static int stat_dummy_5;
+static int stat_dummy_6;
 
 static void f(void) {
 }
