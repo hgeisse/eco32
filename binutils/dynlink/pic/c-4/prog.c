d80e841c1a30d0c66c8a494ad839b4365ade4102
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2018-09-16 00:22:49 +0200

    enhance test c-4

--- binutils/dynlink/pic/c-4/prog.c
+++ binutils/dynlink/pic/c-4/prog.c
@@ -6,7 +6,7 @@ static int s4;
 
 extern int b;
 
-int func1(void) {
+static int func1(void) {
   return 1;
 }
 
@@ -14,8 +14,10 @@ int func2(void) {
   return 2;
 }
 
-void func(void) {
+static void func(void) {
+  /* func1 will be called by a simple 'jal' */
   a += func1();
+  /* func2 will be called by loading $1, followed by 'jalr' */
   b += func2();
   /* read access to variable 'b': GOT-indirect */
   /* write access to varisble 'a': GOT-relative */
