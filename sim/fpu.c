ab81788376f6237436f6144abfdf426696395eb5
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2019-01-10 12:25:51 +0100

    first baby step to integrate FP into simulator

---
+++ sim/fpu.c
@@ -0,0 +1,43 @@
+/*
+ * fpu.c -- floating-point unit
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+#include "console.h"
+#include "error.h"
+#include "fpu.h"
+
+
+Word fpAdd(Word x, Word y) {
+  /* !!!!! FIXME !!!!! */
+  return 0;
+}
+
+
+Word fpSub(Word x, Word y) {
+  /* !!!!! FIXME !!!!! */
+  return 0;
+}
+
+
+Word fpMul(Word x, Word y) {
+  /* !!!!! FIXME !!!!! */
+  return 0;
+}
+
+
+Word fpDiv(Word x, Word y) {
+  /* !!!!! FIXME !!!!! */
+  return 0;
+}
+
+
+Word fpSqrt(Word x) {
+  /* !!!!! FIXME !!!!! */
+  return 0;
+}
---
+++ sim/fpu.h
@@ -0,0 +1,17 @@
+/*
+ * fpu.h -- floating-point unit
+ */
+
+
+#ifndef _FPU_H_
+#define _FPU_H_
+
+
+Word fpAdd(Word x, Word y);
+Word fpSub(Word x, Word y);
+Word fpMul(Word x, Word y);
+Word fpDiv(Word x, Word y);
+Word fpSqrt(Word x);
+
+
+#endif /* _FPU_H_ */
