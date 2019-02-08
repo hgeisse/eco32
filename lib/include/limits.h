a2eef3870802500c575d5ee0925f6a811148f60b
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2019-02-08 08:37:40 +0100

    add definitions in limit.h

--- lib/include/limits.h
+++ lib/include/limits.h
@@ -1,5 +1,5 @@
 /*
- * limits.h -- integral limits
+ * limits.h -- limits of integral types
  */
 
 
@@ -7,7 +7,25 @@
 #define _LIMITS_H_
 
 
-#define LONG_MAX	+2147483647
+#define CHAR_BIT	8
+
+#define UCHAR_MAX	255
+#define SCHAR_MAX	127
+#define SCHAR_MIN	(-SCHAR_MAX - 1)
+#define CHAR_MAX	SCHAR_MAX
+#define CHAR_MIN	SCHAR_MIN
+
+#define USHRT_MAX	65535
+#define SHRT_MAX	32767
+#define SHRT_MIN	(-SHRT_MAX - 1)
+
+#define UINT_MAX	4294967295U
+#define INT_MAX		2147483647
+#define INT_MIN		(-INT_MAX - 1)
+
+#define ULONG_MAX	4294967295UL
+#define LONG_MAX	2147483647L
+#define LONG_MIN	(-LONG_MAX - 1)
 
 
 #endif /* _LIMITS_H_ */
