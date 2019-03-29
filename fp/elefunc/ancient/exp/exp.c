e3f0ddf1caf3a8207ccfd0766af5437796d9f8e7
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2019-03-29 18:29:57 +0100

    update testing of 'ancient' exp

--- fp/elefunc/ancient/exp/exp.c
+++ fp/elefunc/ancient/exp/exp.c
@@ -11,7 +11,7 @@
 #include "../include/fp.h"
 
 
-#define REQ_ULP		66	/* requested accuracy in ulp */
+#define REQ_ULP		18	/* requested accuracy in ulp */
 
 
 /**************************************************************/
@@ -40,118 +40,41 @@ void dump(float x) {
 /**************************************************************/
 
 
-float fp_floor(float x) {
-  _FP_Union X;
-  int expX;
-  _FP_Word mask;
-
-  X.f = x;
-  if ((X.w & 0x7FFFFFFF) == 0) {
-    return X.f;
-  }
-  expX = ((int) _FP_EXP(X.w)) - 127;
-  if (expX < 0) {
-    if (_FP_SGN(X.w)) {
-      X.w = 0xBF800000;
-    } else {
-      X.w = 0x00000000;
-    }
-    return X.f;
-  }
-  if (expX >= 23) {
-    if (expX == 128 && _FP_FRC(X.w) != 0) {
-      X.w |= 0x00400000;
-    }
-    return X.f;
-  }
-  mask = 0x007FFFFF >> expX;
-  if ((X.w & mask) == 0) {
-    return X.f;
-  }
-  if (_FP_SGN(X.w)) {
-    X.w += (0x00800000 >> expX);
-  }
-  X.w &= ~mask;
-  return X.f;
-}
+static float	p0	= .2080384346694663001443843411e7;
+static float	p1	= .3028697169744036299076048876e5;
+static float	p2	= .6061485330061080841615584556e2;
+static float	q0	= .6002720360238832528230907598e7;
+static float	q1	= .3277251518082914423057964422e6;
+static float	q2	= .1749287689093076403844945335e4;
+static float	log2e	= 1.4426950408889634073599247;
+static float	sqrt2	= 1.4142135623730950488016887;
+static float	maxf	= 10000;
 
 
-float fp_ldexp(float x, int n) {
-  float y;
-  _FP_Union M, N;
-  float z;
-
-  y = x;
-  if (n > 127) {
-    M.w = _FP_FLT(0, 127 + 127, 0);
-    y *= M.f;
-    n -= 127;
-    if (n > 127) {
-      y *= M.f;
-      n -= 127;
-      if (n > 127) {
-        n = 127;
-      }
-    }
-  } else
-  if (n < -126) {
-    M.w = _FP_FLT(0, -126 + 127, 0);
-    N.w = _FP_FLT(0, 24 + 127, 0);
-    y *= M.f * N.f;
-    n += 126 - 24;
-    if (n < -126) {
-      y *= M.f * N.f;
-      n += 126 - 24;
-      if (n < -126) {
-        n = -126;
-      }
-    }
-  }
-  M.w = _FP_FLT(0, n + 127, 0);
-  z = y * M.f;
-  return z;
-}
-
-
-/**************************************************************/
-
-
-int debug = 0;
-
-
-static float maxf = 10000;
-static float log2e = 1.4426950408889634073599247;
-static float p0 = 0.2080384346694663001443843411e7;
-static float p1 = 0.3028697169744036299076048876e5;
-static float p2 = 0.6061485330061080841615584556e2;
-static float q0 = 0.6002720360238832528230907598e7;
-static float q1 = 0.3277251518082914423057964422e6;
-static float q2 = 0.1749287689093076403844945335e4;
-static float sqrt2 = 1.4142135623730950488016887;
-
-
-float fp_exp(float x) {
+float fp_exp(float arg) {
   int ent;
-  float fract, xsq;
-  float temp1, temp2;
+  float fract;
+  float xsq;
+  float temp1;
+  float temp2;
 
-  if (x == 0.0) {
+  if (arg == 0.0) {
     return 1.0;
   }
-  if (x < -maxf) {
+  if (arg < -maxf) {
     return 0.0;
   }
-  if (x > maxf) {
+  if (arg > maxf) {
     errno = ERANGE;
     return HUGE_VAL;
   }
-  x *= log2e;
-  ent = fp_floor(x);
-  fract = (x - ent) - 0.5;
+  arg *= log2e;
+  ent = floor(arg);
+  fract = (arg - ent) - 0.5;
   xsq = fract * fract;
   temp1 = ((p2 * xsq + p1) * xsq + p0) * fract;
   temp2 = ((1.0 * xsq + q2) * xsq + q1) * xsq + q0;
-  return fp_ldexp(sqrt2 * (temp2 + temp1) / (temp2 - temp1), ent);
+  return ldexp(sqrt2 * (temp2 + temp1) / (temp2 - temp1), ent);
 }
 
 
@@ -172,6 +95,36 @@ void test_single(float x) {
   printf("r = ");
   dump(r);
   printf("\n");
+}
+
+
+/**************************************************************/
+
+
+void testFew(void) {
+  printf("------------------------------------------------\n");
+  test_single(0.0);
+  printf("------------------------------------------------\n");
+  test_single(1.0);
+  printf("------------------------------------------------\n");
+  test_single(-1.0);
+  printf("------------------------------------------------\n");
+  test_single(2.0);
+  printf("------------------------------------------------\n");
+  test_single(-2.0);
+  printf("------------------------------------------------\n");
+  test_single(1.2345);
+  printf("------------------------------------------------\n");
+  test_single(-1.2345);
+  printf("------------------------------------------------\n");
+  test_single(1.2345e2);
+  printf("------------------------------------------------\n");
+  test_single(-1.2345e2);
+  printf("------------------------------------------------\n");
+  test_single(1.2345e-3);
+  printf("------------------------------------------------\n");
+  test_single(-1.2345e-3);
+  printf("------------------------------------------------\n");
 }
 
 
@@ -238,21 +191,6 @@ void test_many(int count, float lbound, float ubound, int maybeNeg) {
     R.f = r;
     if (_FP_ABS(_FP_DELTA(Z, R)) <= ulp) {
       within++;
-    } else {
-      printf("++++++++++++++++++++\n");
-      printf("x = ");
-      dump(x);
-      printf("\n");
-      debug = 1;
-      z = fp_exp(x);
-      debug = 0;
-      printf("z = ");
-      dump(z);
-      printf("\n");
-      printf("r = ");
-      dump(r);
-      printf("\n");
-      printf("++++++++++++++++++++\n");
     }
   }
   printf("\n");
@@ -266,49 +204,13 @@ void test_many(int count, float lbound, float ubound, int maybeNeg) {
 /**************************************************************/
 
 
-void testFew(void) {
-  printf("------------------------------------------------\n");
-  test_single(0.0);
-  printf("------------------------------------------------\n");
-  test_single(-0.0);
-  printf("------------------------------------------------\n");
-  test_single(2.3);
-  printf("------------------------------------------------\n");
-  test_single(-2.3);
-  printf("------------------------------------------------\n");
-  test_single(2.5);
-  printf("------------------------------------------------\n");
-  test_single(-2.5);
-  printf("------------------------------------------------\n");
-  test_single(2.7);
-  printf("------------------------------------------------\n");
-  test_single(-2.7);
-  printf("------------------------------------------------\n");
-  test_single(3.0);
-  printf("------------------------------------------------\n");
-  test_single(-3.0);
-  printf("------------------------------------------------\n");
-  test_single(1.2345e2);
-  printf("------------------------------------------------\n");
-  test_single(-1.2345e2);
-  printf("------------------------------------------------\n");
-  test_single(1.2345e-3);
-  printf("------------------------------------------------\n");
-  test_single(-1.2345e-3);
-  printf("------------------------------------------------\n");
-}
-
-
-/**************************************************************/
-
-
 void testMany(void) {
   printf("------------------------------------------------\n");
   test_many(10000000, 0.25, 4.0, 1);
   printf("------------------------------------------------\n");
   test_many(10000000, 1.0e-15, 1.0, 1);
   printf("------------------------------------------------\n");
-  test_many(10000000, 1.0, 1.0e15, 1);
+  test_many(10000000, 1.0, 20.0, 1);
   printf("------------------------------------------------\n");
 }
 
@@ -329,9 +231,9 @@ void testAll(int skipSome) {
       printf("reached test 0x%08X\n", i);
     }
     if (skipSome) {
-      while ((i & 0x0003FFC0) != 0x00000000 &&
-             (i & 0x0003FFC0) != 0x0003FFC0) {
-        i += 0x00000040;
+      while ((i & 0x0000FF00) != 0x00000000 &&
+             (i & 0x0000FF00) != 0x0000FF00) {
+        i += 0x00000100;
       }
     }
     X.w = i;
