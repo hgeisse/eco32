9d2ba92a24dfa637ddb4fbc22e7ad98d5d81f06d
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2021-06-05 11:51:09 +0200

    add exercise 09

---
+++ binutils/dynlink/ex09/binutils/dof/disasm.c
@@ -0,0 +1,188 @@
+/*
+ * disasm.c -- disassembler
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "common.h"
+#include "instr.h"
+#include "disasm.h"
+
+
+static char instrBuffer[100];
+
+
+static void disasmN(char *opName, Word immed) {
+  if (immed == 0) {
+    sprintf(instrBuffer, "%-7s", opName);
+  } else {
+    sprintf(instrBuffer, "%-7s %08X", opName, immed);
+  }
+}
+
+
+static void disasmRH(char *opName, int r1, Half immed) {
+  sprintf(instrBuffer, "%-7s $%d,%04X", opName, r1, immed);
+}
+
+
+static void disasmRHH(char *opName, int r1, Half immed) {
+  sprintf(instrBuffer, "%-7s $%d,%08X", opName, r1, (Word) immed << 16);
+}
+
+
+static void disasmRRH(char *opName, int r1, int r2, Half immed) {
+  sprintf(instrBuffer, "%-7s $%d,$%d,%04X", opName, r1, r2, immed);
+}
+
+
+static void disasmRRS(char *opName, int r1, int r2, Half immed) {
+  sprintf(instrBuffer, "%-7s $%d,$%d,%s%04X", opName, r1, r2,
+          SIGN(16) & immed ? "-" : "+",
+          SIGN(16) & immed ? -SEXT16(immed) : SEXT16(immed));
+}
+
+
+static void disasmRRR(char *opName, int r1, int r2, int r3) {
+  sprintf(instrBuffer, "%-7s $%d,$%d,$%d", opName, r1, r2, r3);
+}
+
+
+static void disasmRRB(char *opName, int r1, int r2, Half offset, Word locus) {
+  sprintf(instrBuffer, "%-7s $%d,$%d,%08X", opName,
+          r1, r2, (locus + 4) + (SEXT16(offset) << 2));
+}
+
+
+static void disasmJ(char *opName, Word offset, Word locus) {
+  sprintf(instrBuffer, "%-7s %08X", opName,
+          (locus + 4) + (SEXT26(offset) << 2));
+}
+
+
+static void disasmJR(char *opName, int r1) {
+  sprintf(instrBuffer, "%-7s $%d", opName, r1);
+}
+
+
+static void disasmXN(char *opName) {
+  sprintf(instrBuffer, "%-7s", opName);
+}
+
+
+static void disasmXRR(char *opName, int r1, int r2) {
+  sprintf(instrBuffer, "%-7s $%d,$%d", opName, r1, r2);
+}
+
+
+static void disasmXRRR(char *opName, int r1, int r2, int r3) {
+  sprintf(instrBuffer, "%-7s $%d,$%d,$%d", opName, r1, r2, r3);
+}
+
+
+static void disasmExtended(Instr *ip, Word instr) {
+  Byte xopcode;
+
+  xopcode = instr & 0x000000FF;
+  while (ip != NULL && ip->xopcode != xopcode) {
+    ip = ip->alt;
+  }
+  if (ip == NULL) {
+    disasmN("???", 0);
+  } else {
+    switch (ip->format) {
+      case FORMAT_XN:
+        disasmXN(ip->name);
+        break;
+      case FORMAT_XRR:
+        disasmXRR(ip->name, (instr >> 16) & 0x1F,
+                  (instr >> 21) & 0x1F);
+        break;
+      case FORMAT_XRRR:
+        disasmXRRR(ip->name, (instr >> 11) & 0x1F,
+                   (instr >> 21) & 0x1F, (instr >> 16) & 0x1F);
+        break;
+      default:
+        error("illegal extended entry in instruction table");
+    }
+  }
+}
+
+
+char *disasm(Word instr, Word locus) {
+  Byte opcode;
+  Instr *ip;
+
+  opcode = (instr >> 26) & 0x3F;
+  ip = instrCodeTbl[opcode];
+  if (ip == NULL) {
+    disasmN("???", 0);
+  } else {
+    switch (ip->format) {
+      case FORMAT_N:
+        disasmN(ip->name, instr & 0x03FFFFFF);
+        break;
+      case FORMAT_RH:
+        disasmRH(ip->name, (instr >> 16) & 0x1F, instr & 0x0000FFFF);
+        break;
+      case FORMAT_RHH:
+        disasmRHH(ip->name, (instr >> 16) & 0x1F, instr & 0x0000FFFF);
+        break;
+      case FORMAT_RRH:
+        disasmRRH(ip->name, (instr >> 16) & 0x1F,
+                  (instr >> 21) & 0x1F, instr & 0x0000FFFF);
+        break;
+      case FORMAT_RRS:
+        disasmRRS(ip->name, (instr >> 16) & 0x1F,
+                  (instr >> 21) & 0x1F, instr & 0x0000FFFF);
+        break;
+      case FORMAT_RRR:
+        disasmRRR(ip->name, (instr >> 11) & 0x1F,
+                  (instr >> 21) & 0x1F, (instr >> 16) & 0x1F);
+        break;
+      case FORMAT_RRX:
+        if ((opcode & 1) == 0) {
+          /* the FORMAT_RRR variant */
+          disasmRRR(ip->name, (instr >> 11) & 0x1F,
+                    (instr >> 21) & 0x1F, (instr >> 16) & 0x1F);
+        } else {
+          /* the FORMAT_RRH variant */
+          disasmRRH(ip->name, (instr >> 16) & 0x1F,
+                    (instr >> 21) & 0x1F, instr & 0x0000FFFF);
+        }
+        break;
+      case FORMAT_RRY:
+        if ((opcode & 1) == 0) {
+          /* the FORMAT_RRR variant */
+          disasmRRR(ip->name, (instr >> 11) & 0x1F,
+                    (instr >> 21) & 0x1F, (instr >> 16) & 0x1F);
+        } else {
+          /* the FORMAT_RRS variant */
+          disasmRRS(ip->name, (instr >> 16) & 0x1F,
+                    (instr >> 21) & 0x1F, instr & 0x0000FFFF);
+        }
+        break;
+      case FORMAT_RRB:
+        disasmRRB(ip->name, (instr >> 21) & 0x1F,
+                  (instr >> 16) & 0x1F, instr & 0x0000FFFF, locus);
+        break;
+      case FORMAT_J:
+        disasmJ(ip->name, instr & 0x03FFFFFF, locus);
+        break;
+      case FORMAT_JR:
+        disasmJR(ip->name, (instr >> 21) & 0x1F);
+        break;
+      case FORMAT_XN:
+      case FORMAT_XRR:
+      case FORMAT_XRRR:
+        disasmExtended(ip, instr);
+        break;
+      default:
+        error("illegal entry in instruction table");
+    }
+  }
+  return instrBuffer;
+}
---
+++ binutils/dynlink/ex09/binutils/include/eof.h
@@ -0,0 +1,85 @@
+/*
+ * eof.h -- ECO32 Object Format
+ *          define structure of linkable object and executable files
+ */
+
+
+#ifndef _EOF_H_
+#define _EOF_H_
+
+
+#define EOF_R_MAGIC	0xE7B79BFE	/* relocatable object */
+#define EOF_X_MAGIC	0x3AECCFDF	/* executable object */
+#define EOF_D_MAGIC	0xFB23146C	/* dynamic library object */
+
+#define SEG_ATTR_X	0x01	/* executable */
+#define SEG_ATTR_W	0x02	/* writable */
+#define SEG_ATTR_P	0x04	/* present in object file */
+#define SEG_ATTR_A	0x08	/* allocatable in memory */
+
+#define SYM_ATTR_U	0x01	/* undefined */
+
+#define RELOC_H16	0	/* write 16 bits with high part of value */
+#define RELOC_L16	1	/* write 16 bits with low part of value */
+#define RELOC_R16	2	/* write 16 bits with value relative to PC */
+#define RELOC_R26	3	/* write 26 bits with value relative to PC */
+#define RELOC_W32	4	/* write full 32 bit word with value */
+#define RELOC_GA_H16	5	/* PIC: GOT addr, PC relative, high part */
+#define RELOC_GA_L16	6	/* PIC: GOT addr, PC relative, low part */
+#define RELOC_GR_H16	7	/* PIC: GOT relative, high part */
+#define RELOC_GR_L16	8	/* PIC: GOT relative, low part */
+#define RELOC_GP_L16	9	/* PIC: GOT pointer, offset within GOT */
+#define RELOC_ER_W32	10	/* PIC: executable relative, 32 bit word */
+#define RELOC_SYM	0x100	/* symbol flag, may be added to any RELOC */
+
+
+typedef struct {
+  unsigned int magic;		/* must be one of EOF_<type>_MAGIC */
+  unsigned int osegs;		/* offset of segment table in file */
+  unsigned int nsegs;		/* number of segment table entries */
+  unsigned int osyms;		/* offset of symbol table in file */
+  unsigned int nsyms;		/* number of symbol table entries */
+  unsigned int orels;		/* offset of relocation table in file */
+  unsigned int nrels;		/* number of relocation table entries */
+  unsigned int odata;		/* offset of segment data in file */
+  unsigned int sdata;		/* size of segment data in file */
+  unsigned int ostrs;		/* offset of string space in file */
+  unsigned int sstrs;		/* size of string space in file */
+  unsigned int entry;		/* entry point (if executable) */
+  unsigned int olibs;		/* offset of dynlib names in string space */
+  unsigned int nlibs;		/* number of dynlib names */
+} EofHeader;
+
+
+typedef struct {
+  unsigned int name;		/* offset in string space */
+  unsigned int offs;		/* offset in segment data */
+  unsigned int addr;		/* virtual start address */
+  unsigned int size;		/* size of segment in bytes */
+  unsigned int attr;		/* segment attributes */
+} SegmentRecord;
+
+
+typedef struct {
+  unsigned int name;		/* offset in string space */
+  int val;			/* the symbol's value */
+  int seg;			/* the symbol's segment, -1: absolute */
+  unsigned int attr;		/* symbol attributes */
+} SymbolRecord;
+
+
+typedef struct {
+  unsigned int loc;		/* where to relocate */
+  int seg;			/* in which segment */
+				/* if -1: none, loc is virtual address */
+  int typ;			/* relocation type: one of RELOC_xxx */
+				/* symbol flag RELOC_SYM may be set */
+  int ref;			/* what is referenced */
+				/* if -1: nothing */
+				/* if symbol flag = 0: segment number */
+				/* if symbol flag = 1: symbol number */
+  int add;			/* additive part of value */
+} RelocRecord;
+
+
+#endif /* _EOF_H_ */
