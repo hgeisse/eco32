5aab1f3c3dcd035fb4ebdf1d0f502e4230c98340
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2021-07-03 13:10:55 +0200

    add ls command

--- binutils/dynlink/ex12/NoDOS/system/userland/cmdsrc/ls/ls.c
+++ binutils/dynlink/ex12/NoDOS/system/userland/cmdsrc/ls/ls.c
@@ -3,6 +3,21 @@
  */
 
 
+#include "stdio.h"
+#include "dirent.h"
+
+
 int main(int argc, char *argv[]) {
+  DIR *dp;
+  DirEntry dirEntry;
+
+  dp = opendir();
+  while (1) {
+    if (readdir(&dirEntry, dp) < 0) {
+      break;
+    }
+    printf("%8d  %s\n", dirEntry.byteSize, dirEntry.name);
+  }
+  closedir(dp);
   return 0;
 }
