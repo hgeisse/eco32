7ace0855346f984e70b75f20ba71834e8b00a798
Hellwig Geisse <hellwig.geisse@mni.thm.de>
2021-07-03 13:10:18 +0200

    add cat command

--- binutils/dynlink/ex12/NoDOS/system/userland/cmdsrc/cat/cat.c
+++ binutils/dynlink/ex12/NoDOS/system/userland/cmdsrc/cat/cat.c
@@ -3,6 +3,30 @@
  */
 
 
-int main(int argc, char *argv[]) {
+#include "stdio.h"
+
+
+int cat(char *path) {
+  FILE *fp;
+  char c;
+
+  fp = fopen(path);
+  if (fp == NULL) {
+    printf("cannot open file '%s'\n", path);
+    return 1;
+  }
+  while (fread(&c, 1, 1, fp) == 1) {
+    putchar(c);
+  }
+  fclose(fp);
   return 0;
+}
+
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("missing file name\n");
+    return 1;
+  }
+  return cat(argv[1]);
 }
